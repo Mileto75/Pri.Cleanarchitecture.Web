3fc8fab093b68737572ae05aa29d29a9461fa13d
Mileto Di Marco <mileto.di.marco@howest.be>
2024-02-23 10:30:43 +0100

    Add ProductService

---
+++ Pri.CleanArchitecture.Core/Interfaces/Services/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using Pri.CleanArchitecture.Core.Entities;
+using Pri.CleanArchitecture.Core.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.CleanArchitecture.Core.Interfaces.Services
+{
+    public interface IProductService
+    {
+        Task<ProductResultModel> GetAllAsync(); 
+        Task<ProductResultModel> GetByIdAsync(int id);
+    }
+}
---
+++ Pri.CleanArchitecture.Core/Services/Models/ProductResultModel.cs
@@ -0,0 +1,14 @@
+﻿using Pri.CleanArchitecture.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.CleanArchitecture.Core.Services.Models
+{
+    public class ProductResultModel : BaseResultModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
